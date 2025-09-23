14a195a2b8f883c3a74eee1ce0bfd42130288657
aabh0002 <aabh0002@student.monash.edu>
2025-09-23 10:18:59 +1000

    changed port

--- backend/app.py
+++ backend/app.py
@@ -549,4 +549,4 @@ def map_receipt_route(userid):
 app.register_blueprint(api)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", "5000")), debug=True)
+    app.run(host="0.0.0.0", port="8080", debug=True)

e89da148ecfb64319da1d127e175881f131d3545
mxie0006 <mxie0006@student.monash.edu>
2025-09-23 10:33:49 +1000

    updated port

--- backend/app.py
+++ backend/app.py
@@ -549,4 +549,4 @@ def map_receipt_route(userid):
 app.register_blueprint(api)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="8080", debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
